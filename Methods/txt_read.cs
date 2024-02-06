a909c17a767f4fb7f37a71bbb6753e26475451e3
Atlantasit <marx.leonard@gmx.de>
2024-02-06 14:44:44 +0100

    New Project to waste time :D

---
+++ Methods/txt_read.cs
@@ -0,0 +1,7 @@
+using System;
+using System.IO;
+
+namespace AT_lib{
+    //WIP need to work on something else
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AT_lib{
+
+    class Execution_testing{
+        
+        public static void Main(){
+            key_reader key = new key_reader();
+
+            key.read_key();
+        }
+    }
+}
